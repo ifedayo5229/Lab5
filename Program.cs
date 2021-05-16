87bb1919abc07019417c3a999cf684d31f906c06
Ademodi Seun <seun@zagadat.onmicrosoft.com>
2021-05-15 21:30:21 -0700

    first

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           BankA firtbank = new BankA();
+           firtbank.getBalance();
+           BankB gtbank = new BankB();
+           gtbank.getBalance();
+           BankC unionbank= new BankC();
+           unionbank.getBalance();
+        }
+    }
+
+    public abstract class Bank
+    {
+       public abstract void  getBalance();
+    }
+
+    public class BankA :Bank
+    {
+        public override void getBalance()
+        {
+          
+        Console.WriteLine("your  firstbank balance is $100");
+        }
+
+
+    }
+    public class BankB :Bank
+    {
+        public override void getBalance()
+        {
+           Console.WriteLine("your gtbank balance is $150");
+        }
+
+        
+    }
+    public class BankC: Bank
+    {
+        public override void getBalance()
+        {
+           Console.WriteLine("your unionbank balance is $200");
+        }
+
+        
+    }
+}
